6fe2f8c29399c7a896f9f6d6df41488a07578792
Valdemar_check <valekzhaninvladimir58@gmail.com>
2023-08-23 12:15:08 +0500

    Revert all code

--- src/index.js
+++ src/index.js
@@ -5,31 +5,3 @@ import image4 from "./images/4.jpg";
 import image5 from "./images/5.jpg";
 
 import "./style.css";
-import { Slide } from "./slide";
-
-const slider = document.getElementById("slider");
-console.log("🚀 ~ file: index.js:5 ~ slider:", slider);
-let currentSlide = null;
-let slides = [];
-
-function setupSlide(imageRelativePath) {
-  console.log(
-    "🚀 ~ file: index.js:7 ~ setupSlide ~ imageUrl:",
-    imageRelativePath
-  );
-  const slide = new Slide(imageRelativePath);
-  slides.push(slide);
-  console.log("🚀 ~ file: index.js:10 ~ setupSlide ~ slides:", slides);
-}
-
-setupSlide(image1);
-setupSlide(image2);
-setupSlide(image3);
-setupSlide(image4);
-setupSlide(image5);
-
-render();
-
-function render() {
-  slides.forEach((slide) => slider.appendChild(slide.image));
-}
